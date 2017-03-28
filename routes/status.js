b908b1c51a171b5e4b076a75630995a1ba9936ea
ibeeger <cxh80301983@126.com>
2017-03-28 18:11:45 +0800

    updata status

--- routes/status.js
+++ routes/status.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 var client = require("./tools/client.js");
 
-//获取个人信息
+//获取周边学校
 router.get('/local', function(req, res, next) {
 
 	client.setMethod("GET");
@@ -14,8 +14,26 @@ router.get('/local', function(req, res, next) {
 			res.end(data+"!")
 		}
 	})
-	 
-    
 });
+
+//获取城市景点
+router.get("/jingdian",function(req,res,next){
+	client.setMethod("GET");
+	client.post("http://api.map.baidu.com/geocoder/v2/?location=39.983424,116.322987&output=json&pois=1&ak=C2mrQAjRj3oc9XvbTrPP1UvLEZgWQDZR",{},function(json){
+		let item = {city:"北京"}
+		if (typeof json=='object') {
+			 item["city"] = json["result"]["addressComponent"]["city"];
+			 item["citycode"] = json["result"]["cityCode"];
+		}
+		client.post("http://api.map.baidu.com/place/v2/search?query=景点&region="+item['city']+"&page_size=20&page_num=0&scope=1&output=json&ak=C2mrQAjRj3oc9XvbTrPP1UvLEZgWQDZR",{},function(data){
+			if (typeof data =='object') {
+				res.json(data)
+			}else{
+				res.end(data+"!")
+			}
+		})
+	})
+	
+})
 
 module.exports = router;
