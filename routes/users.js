5d5f3b984cc45d617d3a362913ba198bc20b74c6
崔小邯 <80301983@qq.com>
2017-05-06 10:01:36 +0800

    up

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const WXBizDataCrypt = require('./WXBizDataCrypt')
+const WXBizDataCrypt = require('./wx/WXBizDataCrypt')
 
 // const fetch ={};
 const fetch = require("./tools/fetch.js");
